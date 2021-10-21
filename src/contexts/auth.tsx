fee7dc11ab70afa8e7dcb8c9c73b10381564e81a
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2021-10-21 15:42:21 -0300

    created loginbox and messagelist components

---
+++ src/contexts/auth.tsx
@@ -0,0 +1,87 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+type User = {
+  id: string;
+  name: string;
+  login: string;
+  avatar_url: string;
+}
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+  signOut: () => void;
+}
+
+type AuthProviderProps = {
+  children: ReactNode
+}
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider(props: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=96e7e49e5e41d23d1644`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@dowhile:token', token);
+
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+    setUser(user);
+  }
+
+  function signOut() {
+    setUser(null);
+    localStorage.removeItem('@dowhile:token');
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@dowhile:token');
+
+    if (token) {
+      api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+      api.get<User>('profile').then(response => {
+        setUser(response.data);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGitHubCode = url.includes('?code=');
+
+    if (hasGitHubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=')
+
+      window.history.pushState({}, '', urlWithoutCode);
+
+      signIn(githubCode);
+    }
+
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ signInUrl, user, signOut }}>
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
